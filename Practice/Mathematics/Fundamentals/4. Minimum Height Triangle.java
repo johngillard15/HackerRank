8cf882c330b39f895ab5c908e2ec9f93a2e3697b
johngillard15 <johngillard15@gmail.com>
2021-06-27 17:55:29 -0400

    Create 4. Minimum Height Triangle.java

---
+++ Practice/Mathematics/Fundamentals/4. Minimum Height Triangle.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'lowestTriangle' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER trianglebase
+     *  2. INTEGER area
+     */
+
+    public static int lowestTriangle(int trianglebase, int area) {
+        return (area * 2 + (trianglebase - 1)) / trianglebase;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int trianglebase = Integer.parseInt(firstMultipleInput[0]);
+
+        int area = Integer.parseInt(firstMultipleInput[1]);
+
+        int height = Result.lowestTriangle(trianglebase, area);
+
+        bufferedWriter.write(String.valueOf(height));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
