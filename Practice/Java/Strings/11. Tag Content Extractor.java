dbe169e2271f9fa48faa79c7adddeb01efe2121e
johngillard15 <johngillard15@gmail.com>
2021-06-27 17:18:14 -0400

    Create 11. Tag Content Extractor.java

---
+++ Practice/Java/Strings/11. Tag Content Extractor.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution{
+	public static void main(String[] args){
+		
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases-- > 0){
+			String line = in.nextLine();
+			
+            Pattern regex = Pattern.compile("<(.+)>([^<]+)</\\1>");
+            Matcher match = regex.matcher(line);
+
+            boolean matchFound = false;
+            while(match.find()){
+                System.out.println(match.group(2));
+                matchFound = true;
+            }
+            if(!matchFound)
+                System.out.println("None");
+		}
+	}
+}
